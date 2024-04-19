eed8576416f55fb9bb2b447a2777ef24854f2fdf
Hack-it-Rafi <zozorafi2016@gmail.com>
2024-04-19 15:23:51 +0600

    Fire vfx ready

---
+++ Assets/Main/ThirdPersonController/Scripts/FireScript.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireScript : MonoBehaviour
+{
+    public GameObject firePrefab;
+
+    void OnCollisionEnter(Collision collision)
+    {
+        // Check if the collision is with the log
+        if (collision.gameObject.CompareTag("Log"))
+        {
+            // Get the position of the log
+            Vector3 logPosition = collision.gameObject.transform.position;
+
+            // Instantiate fire at the position of the log
+            Instantiate(firePrefab, logPosition, Quaternion.identity);
+
+            // Deactivate the wood stick GameObject
+            gameObject.SetActive(false);
+
+            // Disable grabbing for the log object
+            ObjectGrabable grabable = collision.gameObject.GetComponent<ObjectGrabable>();
+            if (grabable != null)
+            {
+                grabable.Activate();
+            }
+        }
+    }
+
+
+}
--- Assets/Main/ThirdPersonController/Scripts/ObjectGrabable.cs
+++ Assets/Main/ThirdPersonController/Scripts/ObjectGrabable.cs
@@ -6,24 +6,44 @@ public class ObjectGrabable : MonoBehaviour
 {
     private Rigidbody objectRigidbody;
     private Transform objectGrabPointTransform;
+    private bool activated = false; // Flag to indicate if the object has been activated
 
-    private void Awake(){
+    private void Awake()
+    {
         objectRigidbody = GetComponent<Rigidbody>();
     }
-    public void Grab(Transform objectGrabPointTransform){
-        this.objectGrabPointTransform= objectGrabPointTransform;
-        // objectRigidbody.useGravity = false;
+
+    public void Grab(Transform objectGrabPointTransform)
+    {
+        if (!activated) // Check if the object is not activated
+        {
+            this.objectGrabPointTransform = objectGrabPointTransform;
+            objectRigidbody.useGravity = false;
+        }
     }
 
-    public void Drop(){
+    public void Drop()
+    {
         this.objectGrabPointTransform = null;
-        // objectRigidbody.useGravity = true;
+        objectRigidbody.useGravity = true;
     }
-    private void FixedUpdate() {
-        if(objectGrabPointTransform != null){
+
+    private void FixedUpdate()
+    {
+        if (objectGrabPointTransform != null)
+        {
             float lerpSpeed = 10f;
-            Vector3 newPosition = Vector3.Lerp(transform.position, objectGrabPointTransform.position, Time.deltaTime*lerpSpeed);
+            Vector3 newPosition = Vector3.Lerp(transform.position, objectGrabPointTransform.position, Time.deltaTime * lerpSpeed);
             objectRigidbody.MovePosition(newPosition);
+        }
+    }
+
+    public void Activate()
+    {
+        // Check if the object is tagged as "Log"
+        if (gameObject.CompareTag("Log"))
+        {
+            activated = true;
         }
     }
 }
